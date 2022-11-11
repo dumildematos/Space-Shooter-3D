d13f75d6f655742a95df2fc3711fb8d1dc8566e2
Dumilde Matos <dumildejosematos@gmail.com>
2022-11-11 14:28:52 +0000

    Add ship rotation

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
-    [SerializeField] float movementSpeed = 100;   
+    [SerializeField] float movementSpeed = 50f;  
+    [SerializeField] float trunSpeed = 60f; 
     Transform myT;
 
     void Awake() {
@@ -12,9 +13,17 @@ public class PlayerMovment : MonoBehaviour
     }
 
     void Update() {
+        Turn();
         Thrust();
     }
 
+    void Turn() {
+        float yaw = trunSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float pitch = trunSpeed * Time.deltaTime * Input.GetAxis("Pitch");
+        float roll = trunSpeed * Time.deltaTime * Input.GetAxis("Roll");
+
+        myT.Rotate(-pitch, yaw , -roll);
+    }
     void Thrust(){
         if(Input.GetAxis("Vertical") > 0 )
             myT.position += myT.forward * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");    
